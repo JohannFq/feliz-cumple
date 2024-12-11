3e964f88e0025f86ff1cd4ab769084c7f579ceef
JohannFq <johannquispe2000@gmail.com>
2024-12-11 12:42:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var settings = {
       duration: 2,
       velocity: 100,
       effect: -0.75,
-      size: 30,
+      size: 60,
     },
   };
   
@@ -116,14 +116,11 @@ var settings = {
       duration = settings.particles.duration;
   
     function ParticlePool(length) {
-      // create and populate particle pool
       particles = new Array(length);
       for (var i = 0; i < particles.length; i++) particles[i] = new Particle();
     }
     ParticlePool.prototype.add = function (x, y, dx, dy) {
       particles[firstFree].initialize(x, y, dx, dy);
-  
-      // handle circular queue
       firstFree++;
       if (firstFree == particles.length) firstFree = 0;
       if (firstActive == firstFree) firstActive++;
@@ -131,8 +128,6 @@ var settings = {
     };
     ParticlePool.prototype.update = function (deltaTime) {
       var i;
-  
-      // update active particles
       if (firstActive < firstFree) {
         for (i = firstActive; i < firstFree; i++) particles[i].update(deltaTime);
       }
@@ -141,15 +136,12 @@ var settings = {
           particles[i].update(deltaTime);
         for (i = 0; i < firstFree; i++) particles[i].update(deltaTime);
       }
-  
-      // remove inactive particles
       while (particles[firstActive].age >= duration && firstActive != firstFree) {
         firstActive++;
         if (firstActive == particles.length) firstActive = 0;
       }
     };
     ParticlePool.prototype.draw = function (context, image) {
-      // draw active particles
       if (firstActive < firstFree) {
         for (i = firstActive; i < firstFree; i++)
           particles[i].draw(context, image);
@@ -169,28 +161,27 @@ var settings = {
   (function (canvas) {
     var context = canvas.getContext("2d"),
       particles = new ParticlePool(settings.particles.length),
-      particleRate = settings.particles.length / settings.particles.duration, // particles/sec
+      particleRate = settings.particles.length / settings.particles.duration,
       time;
   
-    // get point on heart with -PI <= t <= PI
     function pointOnHeart(t) {
+      const scale = Math.min(canvas.width, canvas.height) / 800;
       return new Point(
-        160 * Math.pow(Math.sin(t), 3),
-        130 * Math.cos(t) -
-          50 * Math.cos(2 * t) -
-          20 * Math.cos(3 * t) -
-          10 * Math.cos(4 * t) +
-          25
+        scale * 160 * Math.pow(Math.sin(t), 3),
+        scale *
+          (130 * Math.cos(t) -
+            50 * Math.cos(2 * t) -
+            20 * Math.cos(3 * t) -
+            10 * Math.cos(4 * t) +
+            25)
       );
     }
   
-    // creating the particle image using a dummy canvas
     var image = (function () {
       var canvas = document.createElement("canvas"),
         context = canvas.getContext("2d");
       canvas.width = settings.particles.size;
       canvas.height = settings.particles.size;
-      // helper function to create the path
       function to(t) {
         var point = pointOnHeart(t);
         point.x =
@@ -199,40 +190,29 @@ var settings = {
           settings.particles.size / 2 - (point.y * settings.particles.size) / 350;
         return point;
       }
-      // create the path
       context.beginPath();
       var t = -Math.PI;
       var point = to(t);
       context.moveTo(point.x, point.y);
       while (t < Math.PI) {
-        t += 0.01; // baby steps!
+        t += 0.01;
         point = to(t);
         context.lineTo(point.x, point.y);
       }
       context.closePath();
-      // create the fill
       context.fillStyle = "#ea80b0";
       context.fill();
-      // create the image
       var image = new Image();
       image.src = canvas.toDataURL();
       return image;
     })();
   
-    // render that thing!
     function render() {
-      // next animation frame
       requestAnimationFrame(render);
-  
-      // update time
       var newTime = new Date().getTime() / 1000,
         deltaTime = newTime - (time || newTime);
       time = newTime;
-  
-      // clear canvas
       context.clearRect(0, 0, canvas.width, canvas.height);
-  
-      // create new particles
       var amount = particleRate * deltaTime;
       for (var i = 0; i < amount; i++) {
         var pos = pointOnHeart(Math.PI - 2 * Math.PI * Math.random());
@@ -244,7 +224,6 @@ var settings = {
           -dir.y
         );
       }
-  
       particles.update(deltaTime);
       particles.draw(context, image);
     }
@@ -252,13 +231,11 @@ var settings = {
     function onResize() {
       canvas.width = canvas.clientWidth;
       canvas.height = canvas.clientHeight;
+      settings.particles.size = Math.min(canvas.width, canvas.height) / 15;
     }
     window.onresize = onResize;
   
-    setTimeout(function () {
-      onResize();
-      render();
-    }, 10);
+    onResize();
+    render();
   })(document.getElementById("pinkboard"));
-  
   
